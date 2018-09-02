a6729987c906aef6d68deb6b0093ad75948637c2
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-02 02:21:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 43, "M", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "M", "Black", "Hostile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 25, "F", "Brown", "Shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 38, "M", "Black", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 7, "F", "Black", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 18, "M", "Green", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 16, "F", "Brown", "Hostile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9, "M", "Brown", "Murderous", 1);
